05c01ccabc672dd55263b48935a377e6829e84c1
Jérôme Abel <contact@jeromeabel.net>
2024-02-14 13:10:20 +0100

    setup: add netlify adapter ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon({
-      iconDir: 'src/assets/icons',
-    }),
-  ],
+  integrations: [tailwind(), icon({
+    iconDir: 'src/assets/icons'
+  })],
+  output: "server",
+  adapter: netlify()
 });
