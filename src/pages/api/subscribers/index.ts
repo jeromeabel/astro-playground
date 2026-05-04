479296cc7673d648ca54d1c26c21db00af1b96b1
jeromeabel <j.abel@allo-media.fr>
2026-05-04 23:00:00 +0200

    feat: subscribers GET route uses local heroes data

--- src/pages/api/subscribers/index.ts
+++ src/pages/api/subscribers/index.ts
@@ -1,11 +1,8 @@
 import type { APIRoute } from "astro";
+import { heroes } from "../../../data/heroes";
 
 export const prerender = false;
 
-const USERS_URL = "https://jsonplaceholder.typicode.com/users";
-
-export const GET: APIRoute = async () => {
-  const response = await fetch(USERS_URL);
-  const data = await response.json();
-  return new Response(JSON.stringify(data));
+export const GET: APIRoute = () => {
+  return new Response(JSON.stringify(heroes));
 };
